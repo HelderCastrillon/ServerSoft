3b14b8747a81aca7a6e51e09eec7bd668c0bdeeb
HelderCastrillon <heldersoft@gmail.com>
2015-08-26 16:29:48 -0500

    Casi terminado

--- public/modules/administrator/administratorController.js
+++ public/modules/administrator/administratorController.js
@@ -50,27 +50,46 @@ appServersoft.controller('administratorController', ['$scope','$filter','$modal'
 
   }
 
-  $scope.openmodalRegisterActo = function (idUserRegistered) {
+  $scope.openmodalRegisterActo = function (idUserRegistered, typemodal) {
+            
+            if (typemodal == 1) {
+                var modalInstance = $modal.open({
+                    animation: $scope.animationsEnabled,
+                    templateUrl: 'ModalRegisterActo.html',
+                    controller: 'ModalInstanceRegisterActo',
+                    backdrop: false,
+                    size: '',
+                    resolve: {
+                        hpid: function () {
+                            return idUserRegistered;
+                        }
 
-    var modalInstance = $modal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'ModalRegisterActo.html',
-      controller: 'ModalInstanceRegisterActo',
-      backdrop:false,
-      size:'',
-      resolve: {
-        hpid: function () {
-          return idUserRegistered;
-        }
-
-      }
-    });
-
-    modalInstance.result.then(function (DataSave) {
-                $scope.SaveData({ hpid: idUserRegistered }, DataSave);               
-    }, function () {
+                    }
+                });
+                
+                modalInstance.result.then(function (DataSave) {
+                    $scope.SaveData({ hpid: idUserRegistered }, DataSave);
+                }, function () {
       
-    });
+                });
+            }
+            else { 
+                var modalInstance = $modal.open({
+                    animation: $scope.animationsEnabled,
+                    templateUrl: 'ModalGenerarPlano.html',
+                    controller: 'ModalGenerarPlano',
+                    backdrop: false,
+                    size: '',
+                    resolve: {
+                        
+                    }
+                });
+                
+                modalInstance.result.then(function (DataSave) {
+                          }, function () {
+      
+                });
+            }
   };
 
     $scope.alerts = [];
@@ -144,6 +163,71 @@ appServersoft.controller('ModalInstanceRegisterActo', function ($scope, $modalIn
             }
         }
         
+        
+        ////
+         return val;
+    }
+
+});
+
+
+appServersoft.controller('ModalGenerarPlano', function ($scope, $modalInstance) {
+    $scope.Generar = function () {
+
+    };
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+    
+    $scope.maskDate = function (value, sep, pat, nums) {
+        val = value;
+        largo = val.length;
+        var preval = val;
+        val = val.split(sep)
+        
+        if (val.length == 2) {
+            var value = val[1].substring(0, 2);
+            if (value * 1 > 12)
+                return preval.substring(0, preval.length - 1);
+        }
+        if (val.length == 3) {
+            var value = val[2].substring(0, 2);
+            if (value * 1 > 31)
+                return preval.substring(0, preval.length - 1);
+        }
+        
+        val2 = ''
+        for (r = 0; r < val.length; r++) {
+            val2 += val[r]
+        }
+        if (nums) {
+            for (z = 0; z < val2.length; z++) {
+                if (isNaN(val2.charAt(z))) {
+                    letra = new RegExp(val2.charAt(z), "g")
+                    val2 = val2.replace(letra, "")
+                }
+            }
+        }
+        val = ''
+        val3 = new Array()
+        for (s = 0; s < pat.length; s++) {
+            val3[s] = val2.substring(0, pat[s])
+            val2 = val2.substr(pat[s])
+        }
+        
+        for (q = 0; q < val3.length; q++) {
+            if (q == 0) {
+                val = val3[q]
+            }
+            else {
+                if (val3[q] != "") {
+                    val += sep + val3[q]
+                }
+            }
+        }
+        
+        
+        ////
         return val;
     }
 
